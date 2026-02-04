b0316f9dbb70b0898f9cf8ec2f9a07d80afb545c
manideep <manideepkilari2006@gmail.com>
2026-02-04 10:19:21 +0530

    gg

--- IV semester/python/practice/M01_Language_&_Programming_Basis/SO3/PSO7_Array_Basics.py
+++ IV semester/python/practice/M01_Language_&_Programming_Basis/SO3/PSO7_Array_Basics.py
@@ -4,7 +4,8 @@ print(arr,type(arr))
 arr.append(45)
 print(arr)
 arr.append(78)
-print(arr)'''
+print(arr)
+
 import numpy
 arr=numpy.array([12,45,78,23])
 arr1=numpy.array([1,2,3],
@@ -13,3 +14,27 @@ arr1=numpy.array([1,2,3],
 print(arr)
 print(arr1.shape)
 print(arr.dtype)
+'''
+'''
+It is collection of homogenous data elements that can store under a single variable Python does not support arrays
+ 
+Numpy:
+NUmpy-->Numerical Python
+it can easily access arrays
+Mainly uses in ML,DS,AI applications'''
+
+import numpy as np
+arr=np.array([12,45,78,23])
+print(arr)
+print(np.max(arr))
+print(np.min(arr))
+print(np.mean(arr))
+print(np.sum(arr))
+print(np.zeros(5))
+print(np.ones(5))
+print("Even number list is: ",np.arange(2,10,2))
+print("Odd number list is: ",np.arange(1,10,2))
+
+n=int(input("Enter the size of array: "))
+ele=list(map(int,input("Enter the elements of array: ").split()))
+print("Array elements are: ",np.array(ele))
